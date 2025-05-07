47fbbd8da77272a537fbf51bf4e6ae6f2980189d
yezhou2 <jaxyz.ye@mail.utoronto.ca>
2025-05-06 21:09:51 -0400

    static home page

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Item Gallery'),
     );
   }
 }
@@ -47,8 +47,28 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+class Item {
+  final String title;
+  final String imageURI;
+  final bool added;
+
+  Item(this.title, this.imageURI, this.added);
+}
+
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  List<Item> _items = [
+    Item("shoe",
+        "https://static.nike.com/a/images/f_auto,cs_srgb/w_960,c_limit/63c81961-0e99-47d0-a905-7893ca9df07b/men-s-shoes-clothing-accessories.png",
+        false),
+    Item("Nike",
+    "https://static.nike.com/a/images/f_auto/dpr_1.0,cs_srgb/w_1807,c_limit/d4e3b9c4-ed5c-4ab9-b66b-74dbd6ca199a/men-s-shoes-clothing-accessories.png",
+    false),
+    Item("sneakers",
+    "https://static.nike.com/a/images/f_auto/dpr_1.0,cs_srgb/w_1807,c_limit/8599251f-ed87-4442-898c-6ce26b28dfbf/men-s-shoes-clothing-accessories.png",
+    false)
+  ];
+
 
   void _incrementCounter() {
     setState(() {
@@ -74,42 +94,58 @@ class _MyHomePageState extends State<MyHomePage> {
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
+        centerTitle: true,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+      body: GridView.count(
+          crossAxisCount: 2,
+        padding: const EdgeInsets.all(20),
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+        children: _items.map((item) => GalleryCard(imageUrl: item.imageURI, title: item.title)).toList(),
+
       ),
       floatingActionButton: FloatingActionButton(
+        // jump to the shopping cart page
         onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        tooltip: 'shopping cart',
+        child: const Icon(Icons.add_shopping_cart),
       ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class GalleryCard extends StatelessWidget {
+  final String imageUrl;
+  final String title;
+
+  const GalleryCard({required this.imageUrl, required this.title});
+
+  @override
+  Widget build(BuildContext context){
+    return Card(
+      elevation: 4,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+      child: Column(
+        children: [
+          Expanded(child: ClipRRect(
+              borderRadius: BorderRadius.circular(12),
+              child: Image.network(imageUrl, fit: BoxFit.cover)
+          )),
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(20),
+                child: Text(title, textAlign: TextAlign.left,
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20
+                ),),
+              )
+            ],
+          )
+
+        ],
+      ),
     );
   }
 }
