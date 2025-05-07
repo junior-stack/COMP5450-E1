e0fb49c547255e3cba567a246fd56b959296a813
yezhou2 <jaxyz.ye@mail.utoronto.ca>
2025-05-07 19:13:06 -0400

    pop in more data items for the home page

--- lib/data/Item.dart
+++ lib/data/Item.dart
@@ -2,7 +2,8 @@ class Item {
   final String title;
   final String imageURI;
   final String description;
-  bool added;
+  final double price;
+  bool added = false;
 
-  Item(this.title, this.imageURI, this.description, this.added);
+  Item(this.title, this.imageURI, this.description, this.price);
 }
--- lib/page/HomePage.dart
+++ lib/page/HomePage.dart
@@ -22,18 +22,30 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   List<Item> _items = [
-    Item("shoe",
+    Item("General Shoes",
         "https://static.nike.com/a/images/f_auto,cs_srgb/w_960,c_limit/63c81961-0e99-47d0-a905-7893ca9df07b/men-s-shoes-clothing-accessories.png",
-        "shoe",
-        false),
-    Item("Nike",
+        "The Field General returns from its gritty American football roots to shake up the sneaker scene. It pairs synthetic leather and tough textiles with a nubby Waffle sole—creating that vintage gridiron look. Step into a piece of sneaker history and carve your own path into the future.\nColour Shown: Elemental Pink/Gum Medium Brown/Obsidia.\nStyle: FZ5593-600\nCountry/Region of Origin: Vietnam",
+        135),
+    Item("Nike P-6000",
         "https://static.nike.com/a/images/f_auto/dpr_1.0,cs_srgb/w_1807,c_limit/d4e3b9c4-ed5c-4ab9-b66b-74dbd6ca199a/men-s-shoes-clothing-accessories.png",
-        "Nike",
-        false),
-    Item("sneakers",
+        "Look to the future with the Nike P-6000. Refreshing the 2006 Air Pegasus with space-age details, like chrome synthetic leather and metallic fabric, this shoe is ready for whatever's next.\nColour Shown: Metallic Silver/Sail/Black/Metallic Silver\nStyle: CN0149-001\nCountry/Region of Origin: Vietnam\n",
+        30),
+    Item("Air Max Dn8",
         "https://static.nike.com/a/images/f_auto/dpr_1.0,cs_srgb/w_1807,c_limit/8599251f-ed87-4442-898c-6ce26b28dfbf/men-s-shoes-clothing-accessories.png",
-        "sneakers",
-        false)
+        "More Air, less bulk. The Dn8 takes our Dynamic Air system and condenses it into a sleek, low-profile package. Powered by eight pressurised Air tubes, it gives you a responsive sensation with every step. Enter an unreal experience of movement.\nColour Shown: Black/Indigo Burst/Total Orange/Fierce Purple\nStyle: FQ7860-009\nCountry/Region of Origin: Vietnam",
+        60),
+    Item("Pegasus 41",
+    "https://static.nike.com/a/images/f_auto/dpr_1.0,cs_srgb/w_1807,c_limit/898c7faa-3229-4b9d-907b-7995b3a2f0cb/men-s-shoes-clothing-accessories.png",
+    "Responsive cushioning in the Pegasus provides an energised ride for everyday road running. Experience lighter-weight energy return with dual Air Zoom units and a ReactX foam midsole. Plus, improved engineered mesh on the upper decreases weight and increases breathability.\nColour Shown: Black/Anthracite/White\nStyle: FD2722-002\nCountry/Region of Origin: Indonesia, Vietnam",
+    70),
+    Item("Vomero 18",
+    "https://static.nike.com/a/images/f_auto/dpr_1.0,cs_srgb/w_1807,c_limit/40295882-3292-437f-bf2b-4990740447b9/men-s-shoes-clothing-accessories.png",
+    "Maximum cushioning in the Vomero provides a comfortable ride for everyday runs. Our softest, most cushioned ride has lightweight ZoomX foam stacked on top of responsive ReactX foam in the midsole. Plus, a redesigned traction pattern offers a smooth heel-to-toe transition.\nColour Shown: Sail/Hyper Pink/Hyper Pink/Sequoia\nStyle: IB7315-100\nCountry/Region of Origin: Vietnam",
+    45),
+    Item("Air Max Plus",
+    "https://static.nike.com/a/images/f_auto/dpr_1.0,cs_srgb/w_1807,c_limit/f8bdbc8a-f412-483a-8c5e-0fc695cc9e4a/men-s-shoes-clothing-accessories.png",
+    "Let your attitude have the edge in the Air Max Plus. Its iconic caging adds heat to your look while airy mesh keeps you cool. And the visible cushioning lets you celebrate your defiant style in comfort.\nColour Shown: Black/University Gold/White\nStyle: DM0032-013\nCountry/Region of Origin: Indonesia",
+    25)
   ];
 
 
